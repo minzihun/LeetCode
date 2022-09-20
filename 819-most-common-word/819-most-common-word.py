add152c76f10b80ebadb931de43bfc4fbdd60918
Jihun Min <minzihun@gmail.com>
2022-09-20 09:30:22 +0200

    Time: 71 ms (27.99%), Space: 13.9 MB (82.46%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,30 @@
+import re
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        
+        # change upper case to lower case
+        paragraph = paragraph.lower()
+        
+        # remove special words
+        paragraph = re.sub(r'[^\w\s]'," ", paragraph)
+        
+        # remove banned words
+        banned.sort()
+        banned.reverse()
+        for e in banned:
+            paragraph = paragraph.replace(e, "")
+
+        # make a list to split the sentence with space
+        paragraph_list = paragraph.split()
+        
+        # put all elements to dictionary
+        cnt_dic = {}
+        
+        for e in paragraph_list:
+            if e not in cnt_dic:
+                cnt_dic[e] = 1
+            else:
+                cnt_dic[e] += 1
+        
+        return max(cnt_dic, key=cnt_dic.get)
